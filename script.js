c0e5e372f8f37838dc36c1c7ea0e528553670b25
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-14 00:17:44 +0530

    Added the scripts for creating dynamic user card with API data...

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const APIURL = "https://api.github.com/users/";
 
 const form = document.getElementById("form");
+const main = document.getElementById("main");
 const search = document.getElementById("search");
 
 // function getUsers(username) {
@@ -12,10 +13,39 @@ const search = document.getElementById("search");
 async function getUsers(username) {
   try {
     const { data } = await axios(APIURL + username);
-    console.log(data);
+    createUserCard(data);
   } catch (err) {
-    console.log(err);
+    if (err.response.status === 404) {
+      createErrorCard("There is no profile with this user_name...");
+    }
   }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+  <div class="card">
+  <div>
+    <img
+      src="${user.avatar_url}"
+      alt="${user.name}"
+      class="avatar"
+    />
+  </div>
+  <div class="user-info">
+    <h2>${user.name}</h2>
+    <p>${user.bio}</p>
+    <ul>
+      <li>${user.followers}<strong>Followers</strong></li>
+      <li>${user.following}<strong>Following</strong></li>
+      <li>${user.public_repos}<strong>Repos</strong></li>
+    </ul>
+
+    <div id="repos"></div>
+  </div>
+</div>
+  `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener("submit", (e) => {
